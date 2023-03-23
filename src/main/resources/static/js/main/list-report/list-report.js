58c0f8194b9229252ab0b3f74e01fe7b78683065
longnhat197 <longnhat515@gmail.com>
2023-03-23 15:42:43 +0700

    fadeindown modal

--- src/main/resources/static/js/main/list-report/list-report.js
+++ src/main/resources/static/js/main/list-report/list-report.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     // $('.delete').click(function () {
-        
+
     // });
 
-    $(document).on("click", ".delete", function(){
+    $(document).on("click", ".delete", function () {
         let name = $(this).attr("data-name");
         let id = $(this).attr("data-id");
         // console.log(name, id);
@@ -13,8 +13,8 @@ $(document).ready(function () {
             $.ajax({
                 url: "/api/deleteProject/" + id + "/",
                 success: function (result) {
-                    if(result.status == "success") {
-                        $('#'+id).parent("tr").remove();
+                    if (result.status == "success") {
+                        $('#' + id).parent("tr").remove();
                         alertify.success(result.message).delay(2);
                     }
                     if (result.status == "failed") {
@@ -29,21 +29,35 @@ $(document).ready(function () {
     });
 
     tinymce.init({
-        selector:'textarea.tinymce-content',
+        selector: 'textarea.tinymce-content',
         promotion: false,
         ui_container: '#tinymce-group',
-            height: 300,
-            plugins: [
-                'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-                'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-                'insertdatetime', 'media', 'table', 'help', 'wordcount'
-            ],
-            toolbar: 'undo redo | blocks | ' +
-                'bold italic backcolor | alignleft aligncenter ' +
-                'alignright alignjustify | bullist numlist outdent indent | ' +
-                'removeformat | help',
-            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }',
-      });
+        height: 300,
+        plugins: [
+            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+            'insertdatetime', 'media', 'table', 'help', 'wordcount'
+        ],
+        toolbar: 'undo redo | blocks | ' +
+            'bold italic backcolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }',
+    });
+
+    $("#btn-open-import-report-modal").click(function () {
+        const element = document.querySelector('.animatebutton');
+        element.classList.add('animated', 'fadeInDown');
+        setTimeout(function () {
+            element.classList.remove('fadeInDown');
+        }, 800);
+    });
+
+    // $(".btn-close-import-report").click(function(){
+
+    //     $('.animatebutton').fadeOut(1000);
+        
+    // })
 
 
 });
