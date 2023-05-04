e8dc673adcdc2e225f3748fd38e50f3f44b20fff
longnhat197 <longnhat515@gmail.com>
2023-05-04 11:37:47 +0700

    Select2 week, yeark vien-thong and chuyen-doi-so

--- src/main/resources/static/js/main/list-report/list-trien-khai.js
+++ src/main/resources/static/js/main/list-report/list-trien-khai.js
@@ -526,3 +526,40 @@ $("#btn-open-modal-export-report").click(function () {
         }, 2000);
     });
 });
+
+$(document).ready(function () {
+    let params = new URLSearchParams(location.search);
+    week_from_url = params.get('week') ?? 18;
+    year_from_url = params.get('year') ?? 2023;
+    $('#select_week').select2();
+    $('#select_year').select2();
+
+    var week_value = "";
+    var year_value = "";
+
+    for (let i = 2020; i <= 2030; i++) {
+        if (i == year_from_url) {
+            year_value += '<option value="' + i + '" selected>' + i + '</option>';
+        } else {
+            year_value += '<option value="' + i + '">' + i + '</option>';
+        }
+    }
+    document.getElementById("select_year").innerHTML = year_value; // Nhúng HTML cho dữ liệu Select2 (year)
+
+    var value_option_week = "";
+    for (let i = 1; i <= 53; i++) {
+        if (i < 10) {
+            value_option_week = value_option_week + "0" + i;
+        } else {
+            value_option_week = i;
+        }
+
+        if (i == week_from_url) {
+            week_value += '<option value="' + value_option_week + '" selected>' + i + '</option>';
+        } else {
+            week_value += '<option value="' + value_option_week + '">' + i + '</option>';
+        }
+        value_option_week = "";
+    }
+    document.getElementById("select_week").innerHTML = week_value; // Nhúng HTML cho dữ liệu Select2 (week)
+});
